94338237868d59b00cfe228f35a6f0ff2e24f250
Varun <varunjethani@outlook.com>
2024-04-01 23:44:13 +0530

    solve vercel issue 2

--- build_files.sh
+++ build_files.sh
@@ -1,7 +1,9 @@
 
 echo "Build Start"
 python3.9 -m venv venv
-source venv/bin/activate
+source venv/Scripts/activate
+echo "Venv activated"
 pip install -r requirements.txt
+echo "Requirements installed"
 python3.9 manage.py collectstatic --no-input --clear
 echo "Build End"
